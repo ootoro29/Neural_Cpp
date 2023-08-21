5c8a6189cbf4a06a3ad90a98e5d26252feb738db
ootoro29 <gobou256@gmail.com>
2023-08-21 20:14:35 +0900

    layor.h adds

--- inc/Network.h
+++ inc/Network.h
@@ -8,66 +8,9 @@
 #include <climits>
 #include <random>
 #include "Matrix.h"
+#include "layor.h"
 
 namespace Neural{
-    class layor{
-        private:
-            Matrix z,a;
-            Matrix w;
-            Matrix b;
-            Matrix d;
-            Matrix In;
-            int l;
-            double alpha;
-        public:
-            layor(int _l): z(_l,1),a(_l,1),b(_l,1),d(_l,1),w(0,0),In(0,0){
-                l = _l;
-                for(int i = 0; i < _l; i++){
-                    z[i][0] = 0;
-                    a[i][0] = 0;
-                }
-            }
-            int node_cn(){return l;}
-            void set_W(int l1){
-                Matrix m(l,l1);
-                m.set_random();
-                w = m;
-                Matrix n(l,1);
-                n.set_random();
-                b = n;
-            }
-            Matrix fw(const Matrix F){
-                In = F;
-                z = w*F+b;
-                for(int i = 0; i < l; i++){
-                    a[i][0] = f(z[i][0]);
-                }
-                return a;
-            }
-            Matrix bc(const Matrix D,const Matrix W){
-                for(int i = 0; i < l; i++){
-                    double sum = 0;
-                    for(int k = 0; k < D.c; k++){
-                        sum += D[k][0]*W[k][i];
-                    }
-                    d[i][0] = sum;
-                }
-                for(int i = 0; i < l; i++){
-                    d[i][0] *= df(z[i][0]);
-                }
-                return d;
-            }
-            void reset_W(){
-                for(int i = 0; i < l; i++){
-                    for(int j = 0; j < In.c; j++){
-                        w[i][j] -= alpha*d[i][0]*In[j][0];
-                    }     
-                    b[i][0] -= alpha*d[i][0];
-                }
-            }
-            virtual double f(double x);
-            virtual double df(double x);
-    };
     class NetWork{
         private:
             int N;
